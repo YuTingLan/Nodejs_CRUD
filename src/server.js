6df31cf358896b83a1f6bff6f3ae3d96fa2ee356
YuTingLan <fortitude0623@gmail.com>
2023-06-28 23:23:06 +0800

    finished CRUD using MongoDB

--- src/server.js
+++ src/server.js
@@ -21,10 +21,19 @@ app.get('/api/books', async (req, res) => {
 });
 
 // 取得單一資料, api/books/0001
-app.get('/api/books/:bookId', (req, res) => {
+app.get('/api/books/:bookId', async (req, res) => {
     // req.params = /:bookId
     const { bookId } = req.params;
-    const book = books.find((book) => book.id === bookId);
+    // const book = books.find((book) => book.id === bookId);
+
+    const client = await MongoClient.connect(
+        'mongodb://localhost:27017',
+        { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+
+    const db = client.db('nodejs_CRUD');
+    const book = await db.collection('book').findOne({id: bookId});
+
     if(book){
         res.status(200).json(book);
     }
@@ -34,28 +43,50 @@ app.get('/api/books/:bookId', (req, res) => {
 });
 
 // create a new book
-app.post('/api/books', (req, res) => {
+app.post('/api/books', async (req, res) => {
     const { id, name, price, description, imageUrl, averageRating } = req.body;
     const newBook = {  id, name, price, description, imageUrl, averageRating };
 
-    books.push(newBook);
+    const client = await MongoClient.connect(
+        'mongodb://localhost:27017',
+        { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+
+    const db = client.db('nodejs_CRUD');
+    // insertOne 插入新的 obj
+    const books = await db.collection('book').insertOne(newBook);
+
+    // books.push(newBook);
     res.status(200).json('Create Successfully!');
 });
 
 // update a new book
-app.post('/api/books/:bookId', (req, res) => {
+app.post('/api/books/:bookId', async (req, res) => {
     // req.params = /:bookId
     const { bookId } = req.params;
     const { id, name, price, description, imageUrl, averageRating } = req.body;
 
-    const book = books.find((book) => book.id === bookId);
+    const updateBook = {
+        $set: { bookId: id, name: name, price: price, description: description, imageUrl: imageUrl, averageRating: averageRating }
+    };
+
+    const client = await MongoClient.connect(
+        'mongodb://localhost:27017',
+        { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+
+    const db = client.db('nodejs_CRUD');
+    // findOne 只找一筆
+    const book = await db.collection('book').updateOne({id: bookId}, updateBook);
+
+    // const book = books.find((book) => book.id === bookId);
     if(book){
-        book.id = id;
-        book.name = name;
-        book.price = price;
-        book.description = description;
-        book.imageUrl = imageUrl;
-        book.averageRating = averageRating;
+        // book.id = id;
+        // book.name = name;
+        // book.price = price;
+        // book.description = description;
+        // book.imageUrl = imageUrl;
+        // book.averageRating = averageRating;
 
         res.status(200).json('Book Update!');
     }
@@ -65,12 +96,30 @@ app.post('/api/books/:bookId', (req, res) => {
 });
 
 // delete a new book
-app.delete('/api/books/:bookId', (req, res) => {
+app.delete('/api/books/:bookId', async (req, res) => {
     // req.params = /:bookId
     const { bookId } = req.params;
+
+    const client = await MongoClient.connect(
+        'mongodb://localhost:27017',
+        { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+
+    const db = client.db('nodejs_CRUD');
+    // findOne 只找一筆
+    const book = await db.collection('book').findOne({id: bookId});
+
+    if(book){
+        await db.collection('book').deleteOne({id: bookId});
+        res.status(200).json('Delete Successfully!')
+    }
+    else{
+        res.status(404).json('Book Not Found!');
+    }
+
     // 刪除指定的bookId
-    books = books.filter(book => (book.id != bookId))
-    res.status(200).json('Delete Successfully!')
+    // books = books.filter(book => (book.id != bookId))
+    
 });
 
 // 監聽 PORT：8000
